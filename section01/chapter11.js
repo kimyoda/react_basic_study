6374f2f1f5567b94d079cfbab33ccdf57e7ad7f5
kimyoda <rladygks1210@naver.com>
2024-05-02 21:17:06 +0900

    [feat] 01_11_JavaScript Function

--- section01/chapter11.js
+++ section01/chapter11.js
@@ -7,16 +7,17 @@
 
 // greating();
 
-function getArea (width, height) { // 매개변수
-  let area = width * height;
-  function another() {
-    console.log("another");
-  }
+// function getArea (width, height) { // 매개변수
+//   let area = width * height;
+//   function another() { // 중첩 함수
+//     console.log("another");
+//   }
 
-  another();
+//   another();
 
-  return area; // 반환값
-}
+//   return area; // 반환값
+// }
+
 
 let area1 = getArea(10, 20); // 인수
 console.log(area1);
@@ -24,3 +25,19 @@ console.log(area1);
 let area2 = getArea(30, 20);
 console.log(area2);
 getArea(120, 200);
+
+// 호이스팅
+// -> 끌어올리다 라는 뜻 코드의 밑바닥에 작성 된 선언문을 최상단으로 끌어올려서 실행하게 한것을 이야기함
+// 내부적으로 알아서 끌어올려서 실행을 한다.
+//  좀 더 유연하게 코드의 프로그래밍을 작동할 수 있다.
+
+function getArea (width, height) { // 매개변수
+  let area = width * height;
+  function another() { // 중첩 함수
+    console.log("another");
+  }
+
+  another();
+
+  return area; // 반환값
+}
