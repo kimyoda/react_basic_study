8399ab53cd133886ea51f8d340ccb6444abba656
kimyoda <rladygks1210@naver.com>
2024-05-09 00:42:54 +0900

    [feat] 05_05_React Event Handling

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,5 +1,6 @@
 const Button = ({ text, color, children }) => {
   // 이벤트 객체(매개변수)
+  // 합성 이벤트(모든 웹 브라우저의 이벤트 객체를 하나로 통일한 형태)
   const onClickButton = (e) => {
     console.log(e);
     console.log(text);
