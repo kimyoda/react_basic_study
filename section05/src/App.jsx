e8e05acef18ac31ed6c30e62ad9199fc697a6eb0
kimyoda <rladygks1210@naver.com>
2024-05-09 00:29:17 +0900

    [feat] 05_04_React Props Data Send

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,14 +2,23 @@ import "./App.css";
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
+import Button from "./components/Button.jsx";
 
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  };
   return (
     <>
-      {/* Root 컴포넌트 */}
-      <Header />
-      <Main />
-      <Footer />
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
+      <Button text={"블로그"}>
+        <Header />
+      </Button>
     </>
   );
 }
