73c8ad2cd777abc2c1c565e58aa722b6f17aa047
kimyoda <rladygks1210@naver.com>
2024-05-09 16:01:58 +0900

    [feat] 05_10_React useHooks

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,16 +1,10 @@
 import "./App.css";
-import Register from "./components/Register";
-
-// 컴포넌트가 리랜더링 하는 경우는
-//  1. state 값 변경 2. props 값 변경 3. 부모 컴포넌트가 리랜더링 하는 경우
+import HookExam from "./components/HookExam";
 
 function App() {
-  // state = 값, setState = 함수
-
-  // 컴포넌트의 state값이 바뀌면 컴포넌트가 return을 다시 하고 그때 화면이 바뀐다.
   return (
     <>
-      <Register />
+      <HookExam />
     </>
   );
 }
