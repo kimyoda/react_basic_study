daeeceab94bfd71bc939031427e44ec4be4a580f
kimyoda <rladygks1210@naver.com>
2024-05-09 14:55:45 +0900

    [feat] 05_06_React State, Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,34 +1,17 @@
 import "./App.css";
-import { useState } from "react";
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
+
+// 컴포넌트가 리랜더링 하는 경우는 1. state 값 변경 2. props 값 변경 3. 부모 컴포넌트가 리랜더링 하는 경우
 
 function App() {
   // state = 값, setState = 함수
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
 
   // 컴포넌트의 state값이 바뀌면 컴포넌트가 return을 다시 하고 그때 화면이 바뀐다.
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === "ON" ? "OFF" : "ON");
-          }}
-        >
-          {light === "ON" ? "끄기" : "켜기"}
-        </button>
-      </div>
-      <div>
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+      <Bulb />
+      <Counter />
     </>
   );
 }
