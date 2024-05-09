0ec68e2a16cf470c26d3c0aeff81b6ca1ca9d580
kimyoda <rladygks1210@naver.com>
2024-05-10 01:18:23 +0900

    [feat] 08_04_React TodoList App Features Add TodoList

--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,10 +1,39 @@
+import { useState, useRef } from "react";
 import "./Editor.css";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+  // 이벤트 헨들러
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onsubmit();
+    }
+  };
+
+  const onsubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onsubmit}>추가</button>
     </div>
   );
 };
