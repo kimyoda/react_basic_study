f878794b3392e5cdf3234b3c18473882b353dd05
kimyoda <rladygks1210@naver.com>
2024-05-12 16:07:07 +0900

    [feat] 12_03 React Emotion Diary Router Page Move(Link, useNavigate)

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import New from "./pages/New";
@@ -9,15 +9,29 @@ import Notfound from "./pages/Notfound";
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
   return (
-    // Routes 안에는 무조건 Route 컴포넌트만 사용가능하다.
-    // Routes 밖에 있는 컴포넌트들은 전 페이지에 렌더링된다.
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="/*" element={<Notfound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      {/* // Routes 안에는 무조건 Route 컴포넌트만 사용가능하다.
+    // Routes 밖에 있는 컴포넌트들은 전 페이지에 렌더링된다. */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="/*" element={<Notfound />} />
+      </Routes>
+    </>
   );
 }
 
