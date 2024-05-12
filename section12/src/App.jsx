e7b4097ea036be89efa24d130235c28328f9ac14
kimyoda <rladygks1210@naver.com>
2024-05-12 15:26:15 +0900

    [feat] 12_02 React Emotion Diary Router Setting

---
+++ section12/src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
+
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+function App() {
+  return (
+    // Routes 안에는 무조건 Route 컴포넌트만 사용가능하다.
+    // Routes 밖에 있는 컴포넌트들은 전 페이지에 렌더링된다.
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="/*" element={<Notfound />} />
+    </Routes>
+  );
+}
+
+export default App;
