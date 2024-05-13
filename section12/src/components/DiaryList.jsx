8edb6990e9602d51d9e1a1fcb966b511f172c54b
kimyoda <rladygks1210@naver.com>
2024-05-13 11:39:03 +0900

    [feat] 12_09_React Emotion Diary Home UI, Function add

--- section12/src/components/DiaryList.jsx
+++ section12/src/components/DiaryList.jsx
@@ -1,19 +1,48 @@
+import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 import DirayItem from "./DiaryItem";
 import "./DiaryList.css";
+import { useState } from "react";
 
-const DiaryList = () => {
+const DiaryList = ({ data }) => {
+  const nav = useNavigate();
+
+  const [sortType, setSortType] = useState("latest");
+
+  // 이벤트 핸들러
+  const onChangeSortTyep = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedData = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return Number(a.createDate) - Number(b.createDate);
+      } else {
+        return Number(b.createDate) - Number(a.createDate);
+      }
+    });
+  };
+
+  const sortData = getSortedData();
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange={onChangeSortTyep}>
           <option value={"latest"}>최신순</option>
           <option value={"oldest"}>오래된 순</option>
         </select>
-        <Button text={"새 일기 쓰기"} type={"POSITIVE"} />
+        <Button
+          onClick={() => nav("/new")}
+          text={"새 일기 쓰기"}
+          type={"POSITIVE"}
+        />
       </div>
       <div className="list_wrapper">
-        <DirayItem />
+        {sortData.map((item) => (
+          <DirayItem key={item.id} {...item} />
+        ))}
       </div>
     </div>
   );
